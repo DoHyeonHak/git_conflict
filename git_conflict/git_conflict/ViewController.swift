f9e166817fe58d0e00c102d49701df56eef72c3b
Hyeonhak_Do <ehgusgkr6@naver.com>
2024-11-06 22:22:30 +0900

    제발

--- git_conflict/git_conflict/ViewController.swift
+++ git_conflict/git_conflict/ViewController.swift
@@ -2,15 +2,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // mainLabel 부분만 수정하세요!
     private let mainLabel: UILabel = {
-        let label = UILabel()
-        label.text = "안녕하세요"
-        label.textAlignment = .center
-        label.font = .systemFont(ofSize: 24)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
+    let label = UILabel()
+    label.text = "B의 라벨입니다" // 이 부분을 수정
+    label.textAlignment = .center
+    label.font = .systemFont(ofSize: 40) // 이 부분을 수정
+    label.textColor = .blue // 이 부분을 추가
+    label.translatesAutoresizingMaskIntoConstraints = false
+    return label
     }()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
